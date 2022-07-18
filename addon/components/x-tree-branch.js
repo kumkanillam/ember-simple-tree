501ccb77b2b46ec29f1d4773f394a7b9309b1a1c
Rtheeswari <rthyram74@gmail.com>
2022-07-18 19:15:21 +0530

    Overwriting tree view addon for load more action.
    1. Adding onLoad property and onLoad action.
    2.Creating span tag which triggers load more action in tree branch component.

--- addon/components/x-tree-branch.js
+++ addon/components/x-tree-branch.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { get, set } from '@ember/object';
 
 export default class XTreeBranchComponent extends Component {
   get loadMoreText() {
--- addon/components/x-tree.js
+++ addon/components/x-tree.js
@@ -4,13 +4,11 @@ import { get, set } from '@ember/object';
 
 export default class TreeComponent extends Component {
   get collapsedIcon() {
-	  console.log("inside x-tree(this.args)--collapsed-icon", this.args);
     return this.args.collapsedIcon ?? 'x-tree-collapsed-icon';
 
   }
 
   get expandedIcon() {
-	  console.log("inside x-tree(this.args)--expanded-icon", this.args);
     return this.args.expandedIcon ?? 'x-tree-expanded-icon';
   }
   
