14d7ccd2c169e3258d00b38511fd7b9e03cec0be
Rtheeswari <rthyram74@gmail.com>
2022-07-19 19:20:49 +0530

    Removed onLoad method in component.

--- addon/components/x-tree-branch.js
+++ addon/components/x-tree-branch.js
@@ -1,11 +1,3 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
-export default class XTreeBranchComponent extends Component {
-  
-  @action
-   onLoad(model){
-	   this.args.onLoad(model);
-	}
-
-}
+export default class XTreeBranchComponent extends Component {}
