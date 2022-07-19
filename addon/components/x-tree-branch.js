2d66f6d55efb785695a99ca19d9f8444a333e616
Rtheeswari <rthyram74@gmail.com>
2022-07-19 18:50:17 +0530

    Naming convensions

--- addon/components/x-tree-branch.js
+++ addon/components/x-tree-branch.js
@@ -4,8 +4,8 @@ import { action } from '@ember/object';
 export default class XTreeBranchComponent extends Component {
   
   @action
-   onLoad(modell){
-	   this.args.onLoad(modell);
+   onLoad(model){
+	   this.args.onLoad(model);
 	}
 
 }
