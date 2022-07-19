6191545380c5098000eee821337733d53a0d28c7
Rtheeswari <rthyram74@gmail.com>
2022-07-19 11:29:24 +0530

    Avoided the hard coding to obtain the values from external sources.
    1.Appropriate naming conventions.
    2.Removing the unwanted code.

--- addon/components/x-tree-branch.js
+++ addon/components/x-tree-branch.js
@@ -2,14 +2,9 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class XTreeBranchComponent extends Component {
-  get loadMoreText() {
-	if(this.args.model.length !=0){
-		return this.args.model[0].loadMoreText;
-	}
-  }
-   
+  
   @action
-   loadingNext(modell){
+   onLoad(modell){
 	   this.args.onLoad(modell);
 	}
 
